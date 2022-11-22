28d46093a559735ab8ba4561dde5a536fc69923f
Laura Loeschmann <lauraloeschmann@gmx.net>
2022-11-22 17:57:38 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get "/dashboard", to: "pages#dashboard"
   resources :skills do
     collection do
       get :dashboard
