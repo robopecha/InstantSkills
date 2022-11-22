a3ed82272b888795ff68d4fe0babd5dad0f61fc7
Laura Löschmann <44237401+LauraLoe@users.noreply.github.com>
2022-11-22 14:30:00 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Character.create(name: "Luke", movie: movies.first)
  Skill.create!([
   {name: "Coding", description: "Knowledge in python,javaskript and sql", price: 25, level: 2},
-  {name: "Cook a Basic Meal", description: "German and English languages", price: 10, level: 3},
+  {name: "Cook a Basic Italian Meal", description: "First dive into the Italian cuisine", price: 10, level: 3},
   {name: "Moonwalk", description: "Wouldn’t it be cool to be able to glide along the floor as if super-humanly?", price: 30, level: 5}
   {name: "Solve a Rubik Cube", escription: "Rubik cubes seem like the ultimate unsolvable puzzle. But these little cubes are not impossible, as long as you understand the way they work.", price: 5, level: 2}
   {name: "Change a Tire", description: "Knowing how to change a flat tire can be the turning point in making a major issue into merely an inconvenience", price: 7, level: 2}
   {name: "Learn Photography", description: "Being able to take amazing photographs may just make you a favorite among your friends", price: 20, level: 4}
   {name: "How to Meditate", description: "With focus shifting to one well-being, meditation has become more and more common", price: 15, level: 1}
-  {name: "How to Pack a Suitcase", description: "On certain flights, having checked luggage can cost a great deal", price: "5euro", level: "50years experience"}
+  {name: "How to Pack a Suitcase", description: "On certain flights, having checked luggage can cost a great deal", price: 5, level: 5}
   {name: "How to Ride a Bike", description: "With the world’s environment being what it is, learning to ride a bike will not only be a cool skill for you to know but will also be beneficial to the environment", price: 20, level: 4}
   {name: "How to do Basic Home Repairs", description: "Knowing how to repair things around the house will save you both time and money", price: 5, level: 2}
   {name: "How to Organize Your House", description: "Not only will organizing improve the aesthetics of your life, but it will also make your life easier.", price: 10, level: 4}
