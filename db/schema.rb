3b78615d0028688b96fb07ad56ba4d5379007538
Lukas Jerusalem <lukasjerusalem@web.de>
2022-11-24 15:39:30 +0100

    home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_111330) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_105528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_111330) do
     t.datetime "updated_at", null: false
     t.datetime "start_date"
     t.datetime "end_date"
-    t.integer "status", default: 0
     t.index ["skill_id"], name: "index_bookings_on_skill_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
