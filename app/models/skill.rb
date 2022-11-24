b2bf9878dad276e3c498db07003c53001873c0db
Laura Loeschmann <lauraloeschmann@gmx.net>
2022-11-24 14:04:25 +0100

    search+button

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -5,4 +5,11 @@ class Skill < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: true
   validates :level, presence: true, numericality: { in: 1..5 }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
